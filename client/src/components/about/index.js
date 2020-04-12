d0c889d4d5e541502f73617fdd3c184045d4dec8
Alexandru Cotin <a.cotin96@gmail.com>
2020-04-12 17:30:03 +0200

    added proxy

---
+++ client/src/components/about/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+               This is the about page! 
+            </div>
+        );
+    }
+}
+
+export default index;
---
+++ client/src/components/homepage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                This is the homepage!
+            </div>
+        );
+    }
+}
+
+export default index;
